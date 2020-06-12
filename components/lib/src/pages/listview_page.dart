8feb894235250f2c00e652fc08b8efcf306206e3
Alejandro Pareja Londoño <alejopar2012@gmail.com>
2020-06-12 01:45:34 -0500

    Infinite scrolling with futures

---
+++ components/lib/src/pages/listview_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class ListaPage extends StatefulWidget {
+  @override
+  _ListaPageState createState() => _ListaPageState();
+}
+
+class _ListaPageState extends State<ListaPage> {
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+  bool _isLoading = false;
+  ScrollController _scrollController = new ScrollController();
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        fetchData();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista"),
+      ),
+      body: Stack(
+        children: <Widget>[_crearLista(), _crearLoading()],
+      ),
+    );
+  }
+
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[CircularProgressIndicator()],
+            ),
+            SizedBox(height: 15.0,)
+          ]);
+    } else {
+      return Container();
+    }
+  }
+
+  Widget _crearLista() {
+    return ListView.builder(
+        itemCount: _listaNumeros.length,
+        controller: _scrollController,
+        itemBuilder: (BuildContext context, int index) {
+          final image = _listaNumeros[index];
+          return FadeInImage(
+            image: NetworkImage('https://picsum.photos/500/300/?images=$image'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+          );
+        });
+  }
+
+  void _agregar10() {
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {});
+  }
+
+  Future fetchData() async {
+    _isLoading = true;
+    setState(() {});
+    final duration = new Duration(seconds: 2);
+    new Timer(duration, respuestaHttp);
+  }
+
+  void respuestaHttp() {
+    _isLoading = false;
+    _scrollController.animateTo(_scrollController.position.pixels+20, duration: 
+    Duration(milliseconds: 250), curve: Curves.fastOutSlowIn);
+    _agregar10();
+  }
+}
--- components/lib/src/utils/icono_string_util.dart
+++ components/lib/src/utils/icono_string_util.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
-final _icons =<String,IconData> {
- 'add_alert': Icons.add_alert,
- 'accessibility': Icons.accessibility,
+final _icons = <String, IconData>{
+  'add_alert': Icons.add_alert,
+  'accessibility': Icons.accessibility,
   'folder_open': Icons.folder_open,
   'donut_large': Icons.donut_large,
   'input': Icons.input,
-   'list': Icons.list,
-   'tune':Icons.tune
+  'list': Icons.list,
+  'tune': Icons.tune
 };
 
-Icon getIcon(String nombreIcono){
-  return Icon(_icons[nombreIcono], color: Colors.blue,);
+Icon getIcon(String nombreIcono) {
+  return Icon(
+    _icons[nombreIcono],
+    color: Colors.blue,
+  );
 }
